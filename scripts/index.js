c260424d09d59d76576aa9edceae4368c4adfcad
fungj <fungj@reed.edu>
2018-11-27 16:54:41 -0800

    data, data, data

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,18 @@
-const input = document.getElementById('inputGroupFileAddon01');
+const input = document.getElementById('file-input');
+const inputLabel = document.getElementById('file-input-label');
+
+const process = async ({meta, data}) => {
+  // empty res file with metadata attached
+  console.log(meta);
+  const res = { meta };
+  const nFish = meta[4].value;  
+  const output = math.zeros(nFish, nFish);
+  const agressorKeys = data.map(d => d.fish1 );
+  const fleerKeys = data.map(d => d.fish2);
+  const fishKeysUnsorted =  agressorKeys.concat(fleerKeys);
+  const fishKeys = fishKeysUnsorted.sort();
+  console.log(fishKeys); 
+};
 
 const parseMeta = async (text) => {
   const regex = /Answer.([0-9])=(.+)/g;
@@ -34,15 +48,19 @@ const parseData = async (text) => {
 const parse = async (text) => {
   const meta = await parseMeta(text);
   const data = await parseData(text);
-  console.log(meta, data);
+  const results = await process({ meta, data });
+  console.log(results);
 };
 
 const handleFileSelect = (event) => {
+  const fileNames = [];
   for (file of event) {
+    fileNames.push(file.name);
     const reader = new FileReader();
     reader.onload = async (e) => {
       const results = await parse(e.target.result);
     };
     reader.readAsText(file);
   };
+  inputLabel.innerHTML = JSON.stringify(fileNames);
 }
