f733ed980dba94d6901aeef21394b183c5b28114
fungj <fungj@reed.edu>
2018-11-29 16:48:22 -0800

    Updates

---
+++ precache-manifest.426ffe5d5040d59500b9220f1ba910e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d449e68ef3eac4c82dc7",
+    "url": "/wranglefish/static/js/runtime~main.d449e68e.js"
+  },
+  {
+    "revision": "23f8e4ec41e6a9cd7bd0",
+    "url": "/wranglefish/static/js/main.23f8e4ec.chunk.js"
+  },
+  {
+    "revision": "d4bd161e69309250fc7d",
+    "url": "/wranglefish/static/js/1.d4bd161e.chunk.js"
+  },
+  {
+    "revision": "d4bd161e69309250fc7d",
+    "url": "/wranglefish/static/css/1.c74a9aa9.chunk.css"
+  },
+  {
+    "revision": "1f7eb0b1133e2e1c84df07860e265abd",
+    "url": "/wranglefish/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e94ca02229e3b3b04d610b4e463f051.js"
+  "/wranglefish/precache-manifest.426ffe5d5040d59500b9220f1ba910e6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/wranglefish/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
